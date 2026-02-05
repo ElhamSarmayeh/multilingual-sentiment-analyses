1c821677b28ed2a83e57aa69c5623622a088898c
ElhamSarmayeh <sarmayehelham@gmail.com>
2026-02-05 03:48:43 +0330

    Update app.py
    Defining xlm-roberta model
    
    Defining xlm-roberta task
    
    Defining function for model
    
    Creating ui with gradio

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import transformers as tf 
-import gradio as a gr
+import gradio as gr
 from transformers import pipeline 
+
+
+model = pipeline ("sentiment-analysis",model =
+"cardiffnlp/twitter-xlm-sentiment-multilingual")
+
+def analyze (text) :
+  result = model(text)[0]
+return f"{result['label']} , {result['score']:.0% }"
+
+gr.Interface(fn=analyze,inputs="text",outputs="text").launch()
